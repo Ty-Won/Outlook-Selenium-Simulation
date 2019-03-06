443d6e5be20f4c2298dec3ea7b5a4ea42cf6b94a
AlexLam1997 <alexander.lam@mail.mcgill.ca>
2019-03-06 14:36:15 -0500

    structure for cucumber tests

---
+++ src/test/java/com/outlook/cucumber/StepDefinition.java
@@ -0,0 +1,35 @@
+package com.outlook.cucumber;
+
+import cucumber.api.java8.En;
+
+public class StepDefinition implements En{
+  public StepDefinition() {
+
+    Given("I am on the McGill Outlook Email page", () -> {
+      // Write code here that turns the phrase above into concrete actions
+      throw new cucumber.api.PendingException();
+    });
+
+    Given("the email contains all the correct credentials", () -> {
+      // Write code here that turns the phrase above into concrete actions
+      throw new cucumber.api.PendingException();
+    });
+
+    Given("the email has an image attached", () -> {
+      // Write code here that turns the phrase above into concrete actions
+      throw new cucumber.api.PendingException();
+    });
+
+    When("I click {string}", (String string) -> {
+      // Write code here that turns the phrase above into concrete actions
+      throw new cucumber.api.PendingException();
+    });
+
+    Then("the email should be sent with the photo", () -> {
+      // Write code here that turns the phrase above into concrete actions
+      throw new cucumber.api.PendingException();
+    });
+
+
+  }
+}
